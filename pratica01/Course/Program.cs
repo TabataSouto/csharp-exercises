216bd1387da043a7314d8bc9c4be008a0087e9c9
Tabata Souto <tabatac.souto@gmail.com>
2022-08-28 14:35:22 -0300

    feat: exemplo tipos de variável valor

---
+++ pratica01/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course;
+
+class Program {
+  static void Main(string[] args) {
+    sbyte n1 = 100;
+    Console.WriteLine(n1);
+
+    // não aceita valores negativos
+    byte n2 = 254;
+    // adiciona mais um, onde o valor passa a vale 255
+    n2++;
+    Console.WriteLine(n2);
+
+    int n3 = 1000;
+    Console.WriteLine(n3);
+  }
+}
