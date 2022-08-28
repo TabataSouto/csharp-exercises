10e7f42ac93372d5754aadaee2219e9d66841631
Tabata Souto <tabatac.souto@gmail.com>
2022-08-28 19:22:11 -0300

    feat: função de soma

---
+++ EstruturaSequencial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EstruturaSequencial;
+
+class DataInputAndOutput {
+  static void Main() {
+    int numberOne, numberTwo, sum;
+
+    numberOne = int.Parse(Console.ReadLine());
+    numberTwo = int.Parse(Console.ReadLine());
+
+    sum = numberOne + numberTwo;
+
+    Console.WriteLine($"SOMA = {sum}");
+  }
+};
