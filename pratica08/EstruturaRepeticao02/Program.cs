b2e16ffc4f1cb48791e4f1649d16cc1862c422e8
Tabata Souto <tabatac.souto@gmail.com>
2022-08-29 11:44:19 -0300

    feat: leitura dos valores ímpares de acordo com número informado.

---
+++ pratica08/EstruturaRepeticao02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EstruturaRepeticao02;
+
+public class Program {
+  static void Main() {
+    /* Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre
+    os ímpares de 1 até X, um valor por linha, inclusive o
+    X, se for o caso. */
+
+    int value = Convert.ToInt32(Console.ReadLine());
+    for (int i = 1; i <= value; i++) {
+      if (i % 2 == 1) {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
