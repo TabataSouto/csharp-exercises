03d1b3da49f7f38021c28e1a540c0c4fc662ff81
Tabata Souto <tabatac.souto@gmail.com>
2022-08-29 11:57:08 -0300

    feat: contagem de números dentro do intervalo de 10 a 20

---
+++ pratica08/EstruturaRepeticao03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EstruturaRepeticao03;
+
+public class Program {
+  static void Main() {
+    /* Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+      Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+      essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo). */
+
+    int value = Convert.ToInt32(Console.ReadLine());
+    int inQtde = 0;
+    int outQtde = 0;
+
+    for (int i = 0; i < value; i++) {
+      int number = Convert.ToInt32(Console.ReadLine());
+      if (number >= 10 && number <= 20) {
+        inQtde++;
+      } else {
+        outQtde++;
+      }
+    }
+
+    Console.WriteLine($"{inQtde} in");
+    Console.WriteLine($"{outQtde} in");
+  }
+}
