311a9494cde2ebbd9b94f7c4590964e8a08534bb
Tabata Souto <tabatac.souto@gmail.com>
2022-08-29 11:11:10 -0300

    feat: leitura de abastecimento de combustível.

---
+++ pratica07/EstruturaWhile03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaWhile03;
+
+public class FuelFilled {
+  static void Main() {
+    int code = int.Parse(Console.ReadLine());
+    int alcohol = 0;
+    int gasoline = 0;
+    int diesel = 0;
+    
+    while (code != 4) {
+      if (code == 1) alcohol++;
+      if (code == 2) gasoline++;
+      if (code == 3) diesel++;
+      else code = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("MUITO OBRIGADO");
+    Console.WriteLine($"Alcool: {alcohol}");
+    Console.WriteLine($"Gasolina: {gasoline}");
+    Console.WriteLine($"Diesel: {diesel}");
+  }
+}
