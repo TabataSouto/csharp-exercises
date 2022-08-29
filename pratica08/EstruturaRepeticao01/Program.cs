80fead839f8b77c6fdf26a215d05b6e2ed214aff
Tabata Souto <tabatac.souto@gmail.com>
2022-08-29 11:35:41 -0300

    feat: soma de valores com repetição.

---
+++ pratica08/EstruturaRepeticao01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace EstruturaRepeticao01;
+
+class Program {
+  static void Main() {
+    Console.Write("Quantos números inteiros você vai digitar? ");
+
+    int number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++) {
+      Console.Write("Valor: #{0}:", i);
+      int value = Convert.ToInt32(Console.ReadLine());
+      sum += value;
+    }
+
+    Console.WriteLine($"Soma = {sum}");
+  }
+}
