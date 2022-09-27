aaa8c89bea98819a221d7e6989884a46d7b795bc
Tabata Souto <tabatac.souto@gmail.com>
2022-09-27 11:41:48 -0300

    NegativeNumber: retorna se o número informado é negativo ou não.

---
+++ pratica07/EstruturaCondicionalIfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstruturaCondicionalIfElse;
+
+class NegativeNumber {
+  static void Main() {
+    int number;
+
+    Console.Write("Digite um número: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number < 0 ) {
+      Console.WriteLine("NEGATIVO");
+    } else {
+      Console.WriteLine("NÃO NEGATIVO");
+    }
+    
+  }
+}
