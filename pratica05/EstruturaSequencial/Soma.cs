f9356e38df0ba69231e476be386a83f8317f647f
Tabata Souto <tabatac.souto@gmail.com>
2022-09-27 10:29:12 -0300

    Lê o valor do raio de um círculo para depois mostrar o valor da área deste círculo com quatro casas decimais.

---
+++ pratica05/EstruturaSequencial/Soma.cs
@@ -0,0 +1,14 @@
+﻿namespace EstruturaSequencial;
+
+class DataInputAndOutput {
+  static void Main() {
+    int numberOne, numberTwo, sum;
+
+    numberOne = int.Parse(Console.ReadLine());
+    numberTwo = int.Parse(Console.ReadLine());
+
+    sum = numberOne + numberTwo;
+
+    Console.WriteLine($"SOMA = {sum}");
+  }
+};
---
+++ pratica05/EstruturaSequencial02/CircleCalc.cs
@@ -0,0 +1,17 @@
+﻿namespace EstruturaSequencial02;
+using System.Globalization;
+
+class CircleCalc {
+    static void Main() {
+      // calculo: (area * tt) * 2
+      double area = 0;
+      double tt = 3.14159;
+
+      Console.Write("Digite um número: ");
+      area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+      double calc = (area * tt) * area;
+
+      Console.WriteLine($"A = {calc.ToString("F4", CultureInfo.InvariantCulture)}");
+    }
+}
