f1748327226f4c9f0f724d4798e74a0c7416a58d
Tabata Souto <tabatac.souto@gmail.com>
2022-09-27 10:35:39 -0300

    Programa para ler quatro valores inteiros A, B, C e D. É feito o calculo para mostrar a diferença do produto de A e B pelo produto de C e D.

--- pratica05/EstruturaSequencial02/CircleCalc.cs
+++ pratica05/EstruturaSequencial02/CircleCalc.cs
@@ -1,17 +1,36 @@
 ﻿namespace EstruturaSequencial02;
 using System.Globalization;
 
-class CircleCalc {
+// class CircleCalc {
+//     static void Main() {
+//       // calculo: (area * tt) * 2
+//       double area = 0;
+//       double tt = 3.14159;
+
+//       Console.Write("Digite um número: ");
+//       area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//       double calc = (area * tt) * area;
+
+//       Console.WriteLine($"A = {calc.ToString("F4", CultureInfo.InvariantCulture)}");
+//     }
+// }
+
+class ProductValueDifference {
     static void Main() {
-      // calculo: (area * tt) * 2
-      double area = 0;
-      double tt = 3.14159;
+      int A, B, C, D, calc;
 
-      Console.Write("Digite um número: ");
-      area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      Console.Write("Digite o primeiro valor: ");
+      A = int.Parse(Console.ReadLine());
+      Console.Write("Digite o segundo valor: ");
+      B = int.Parse(Console.ReadLine());
+      Console.Write("Digite o terceiro valor: ");
+      C = int.Parse(Console.ReadLine());
+      Console.Write("Digite o quarto valor: ");
+      D = int.Parse(Console.ReadLine());
 
-      double calc = (area * tt) * area;
+      calc = (A * B) - (C * D);
 
-      Console.WriteLine($"A = {calc.ToString("F4", CultureInfo.InvariantCulture)}");
+      Console.WriteLine($"DIFERENÇA = {calc}");
     }
 }
